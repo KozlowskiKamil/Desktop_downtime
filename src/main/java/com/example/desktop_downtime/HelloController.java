177379ec109ecd71c818363be8cade0e903b3e8f
KozlowskiKamil <kamil2k8@gmail.com>
2023-08-07 13:22:09 +0200

    add one button to star and stop

--- src/main/java/com/example/desktop_downtime/HelloController.java
+++ src/main/java/com/example/desktop_downtime/HelloController.java
@@ -1,26 +1,49 @@
 package com.example.desktop_downtime;
 
 import javafx.fxml.FXML;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 
 import java.io.IOException;
 
 public class HelloController {
+
     @FXML
     private Label welcomeText;
 
     @FXML
-    protected void onSendButtonClick() throws IOException {
+    private Button endButton;
+
+    private boolean isEndButtonVisible = false;
+
+    @FXML
+    public void onSendEndButtonClick() throws IOException {
+        if (isEndButtonVisible) {
+            // Wykonaj akcję przypisaną do przycisku "Zamknij awarię"
+            endButtonClick();
+            // Zmień etykietę i przypisaną akcję przycisku na "Zgłoś awarię"
+            welcomeText.setText("Witaj w aplikacji Desktop Downtime!");
+            endButton.setText("Zgłoś awarię");
+            isEndButtonVisible = false;
+        } else {
+            // Wykonaj akcję przypisaną do przycisku "Zgłoś awarię"
+            onSendButtonClick();
+            // Zmień etykietę i przypisaną akcję przycisku na "Zamknij awarię"
+            welcomeText.setText("Zgłoszono awarię!");
+            endButton.setText("Zamknij awarię");
+            isEndButtonVisible = true;
+        }
+    }
+
+
+    private void onSendButtonClick() throws IOException {
         welcomeText.setText("Wysłano zgłoszenie awarii");
-// Wywołaj metodę sendBreakdown() z klasy HelloApplication
         BreakdownService breakdownService = new BreakdownService();
         breakdownService.onSendButtonClick();
     }
 
-    @FXML
-    protected void endButtonClick() throws IOException {
-        welcomeText.setText("Wysłano zgłoszenie awarii");
-// Wywołaj metodę editBreakdown() z klasy HelloApplication
+    private void endButtonClick() {
+        welcomeText.setText("Awaria została zamknięta");
         BreakdownService breakdownService = new BreakdownService();
         breakdownService.endButtonClick();
     }
